46afdcb7f23401fdb354c320197324c27072e420
DDred <avs110@inbox.ru>
2010-11-30 06:36:00 +0000

    Event Test added

--- src/libiada/EventTheory/Place.java
+++ src/libiada/EventTheory/Place.java
@@ -1,7 +1,10 @@
 package libiada.EventTheory;
 
+import libiada.Root.IBaseObject;
+import libiada.Root.IBin;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.List;
 
 /*
  * To change this template, choose Tools | Templates
@@ -12,7 +15,8 @@ import java.util.List;
  *
  * @author Алексе
  */
-public class Place {
+public class Place implements IBaseObject
+{
     private ArrayList<Dimension> pDimensions;
 
     public Place(ArrayList<Dimension> dimensions) {
@@ -26,6 +30,32 @@ public class Place {
         return new long[1];
     }
 
-    public void setValue(int value, int index) {
+    public void setValue(long value, int index) {
+    }
+
+    public Place setValues(long[] value) throws Exception {
+        if (Array.getLength(value) != pDimensions.size())
+            {
+                throw new Exception("Размерности не совпадают");
+            }
+
+            for (int i = 0; i < pDimensions.size(); i++)
+            {
+                setValue(value[i], i);
+            }
+
+            return this;
+    }
+
+    public IBaseObject Clone() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public boolean Equals(Object obj) {
+        return false;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public IBin GetBin() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 }
