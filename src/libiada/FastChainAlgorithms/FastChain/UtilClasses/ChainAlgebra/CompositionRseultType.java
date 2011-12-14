46e9c2976bcdab41ec37c279ab6a14d2af6641d3
Alex Semenov <avs110@inbox.ru>
2011-12-14 20:55:27 +0700

    jlibiada-rapidminer-mysql files added and rep created

--- src/libiada/FastChainAlgorithms/FastChain/UtilClasses/ChainAlgebra/CompositionRseultType.java
+++
@@ -1,9 +0,0 @@
-package libiada.FastChainAlgorithms.FastChain.UtilClasses.ChainAlgebra;
-
-/**
- * Created by IntelliJ IDEA.
- * User: Alex
- * Date: 08.08.11
- * Time: 18:09
- */
-public enum CompositionRseultType { Unifom, NotUniform; }
